2c4a629e97dac0fda0393fa808f3e23bf24ccb1c
0xOmarA <OmarAbdulla7@hotmail.com>
2022-08-23 17:03:43 +0300

    Switch from npm to yarn

--- out/formatting_providers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BasicFormattingProvider = void 0;
-var basic_formatting_provider_1 = require("./basic_formatting_provider");
-Object.defineProperty(exports, "BasicFormattingProvider", { enumerable: true, get: function () { return basic_formatting_provider_1.default; } });
-//# sourceMappingURL=index.js.map
--- out/hover_providers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BasicHoverProvider = void 0;
-var basic_hover_provider_1 = require("./basic_hover_provider");
-Object.defineProperty(exports, "BasicHoverProvider", { enumerable: true, get: function () { return basic_hover_provider_1.default; } });
-//# sourceMappingURL=index.js.map
